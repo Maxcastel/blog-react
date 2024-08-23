aa9db414eccd12aec1ab7f9586abd0131015679b
Maxcastel <maxence.nord@gmail.com>
2024-08-23 16:14:46 +0200

    App: wrap routes with Layout &  LayoutAdmin and wrap all with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Layout } from './components/layout/Layout'
+import { Test } from './components/Test'
+import { ThemeProvider } from './components/theme-provider'
+import { LayoutAdmin } from './components/admin/layout/LayoutAdmin'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Test />} />
+          </Route>
+          <Route element={<LayoutAdmin />}>
+            <Route path="/a" element={<Test />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
