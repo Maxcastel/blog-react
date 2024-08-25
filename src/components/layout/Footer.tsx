2c019fa6b98679f64f980abceb3fc7383d007043
Maxcastel <maxence.nord@gmail.com>
2024-08-25 23:02:08 +0200

    Footer: add translations

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,10 @@
+import { useTranslation } from "react-i18next";
 import CV from "../../assets/CV.pdf"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export function Footer(){
+    const { t } = useTranslation();
 
     return (
         <footer className="flex justify-between py-4">
@@ -10,18 +12,18 @@ export function Footer(){
                 <h3 className="text-lg font-medium mb-1">Contact</h3>
                 <ul>
                     <li>
-                        <a href="#contact">Contactez-moi</a>
+                        <a href="#contact">{t('footer.contactme')}</a>
                     </li>
                     <li>
-                        <a href={CV} target="_blank" rel="noreferrer">Voir mon CV</a>
+                        <a href={CV} target="_blank" rel="noreferrer">{t('footer.seemycv')}</a>
                     </li>
                 </ul>
             </div>
             <div className="flex items-end">
-                <p>&copy; {(new Date()).getFullYear()} Maxence Castel. Tous droits réservés.</p>
+                <p>&copy; {(new Date()).getFullYear()} Maxence Castel. {t('footer.allrightsreserved')}</p>
             </div>
             <div>
-                <h3 className="text-lg font-medium mb-2">Suivez-moi</h3>
+                <h3 className="text-lg font-medium mb-2">{t('footer.followme')}</h3>
                 <ul className="flex justify-center gap-x-3">
                     <li>
                         <a href="https://github.com/Maxcastel" target="_blank" rel="noreferrer">
