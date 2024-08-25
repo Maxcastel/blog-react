bd00e557d859eb5c5ec91dd8887b2a6ab12d9951
Maxcastel <maxence.nord@gmail.com>
2024-08-25 22:59:19 +0200

    add Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,40 @@
+import CV from "../../assets/CV.pdf"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+export function Footer(){
+
+    return (
+        <footer className="flex justify-between py-4">
+            <div className="">
+                <h3 className="text-lg font-medium mb-1">Contact</h3>
+                <ul>
+                    <li>
+                        <a href="#contact">Contactez-moi</a>
+                    </li>
+                    <li>
+                        <a href={CV} target="_blank" rel="noreferrer">Voir mon CV</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="flex items-end">
+                <p>&copy; {(new Date()).getFullYear()} Maxence Castel. Tous droits réservés.</p>
+            </div>
+            <div>
+                <h3 className="text-lg font-medium mb-2">Suivez-moi</h3>
+                <ul className="flex justify-center gap-x-3">
+                    <li>
+                        <a href="https://github.com/Maxcastel" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faGithub} size="xl" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/maxence-castel-46391a224/" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faLinkedin} size="xl" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
