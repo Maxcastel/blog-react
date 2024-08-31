861684bf99ede181bb586d3ddaf9b2c41ff37b81
Maxcastel <maxence.nord@gmail.com>
2024-08-31 19:28:01 +0200

    App: add /admin/articles/create route (CreateArticle)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Home } from './components/home/Home'
 import { Articles } from './components/article/Articles'
 import { ShowArticle } from './components/article/ShowArticle'
 import { Articles as ArticlesAdmin } from './components/admin/article/Articles'
+import { CreateArticle } from './components/admin/article/CreateArticle'
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/admin" element={<LayoutAdmin />}>
             <Route index element={<Home />} />
             <Route path="articles" element={<ArticlesAdmin />} />
+            <Route path="articles/create" element={<CreateArticle />} />
           </Route> 
         </Routes>
       </BrowserRouter>
